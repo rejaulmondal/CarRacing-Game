79c588a6f86ac2aab684e89e0e1b40684461ceb6
rejaulmondal <rejaulmondal786@gmail.com>
2020-04-15 16:52:56 +0530

    CarRacing Game

--- js/Game.js
+++ js/Game.js
@@ -55,7 +55,7 @@ class Game {
       var index = 0;
 
       //x and y position of the cars
-      var x = 175 ;
+      var x = 320 ;
       var y;
 
       for(var plr in allPlayers){
@@ -63,13 +63,16 @@ class Game {
         index = index + 1 ;
 
         //position the cars a little away from each other in x direction
-        x = x + 200;
+        x = x + 272;
         //use data form the database to display the cars in y direction
         y = displayHeight - allPlayers[plr].distance;
         cars[index-1].x = x;
         cars[index-1].y = y;
 
         if (index === player.index){
+          stroke(10);
+          fill("red");
+          ellipse(x,y,60,60);
           cars[index - 1].shapeColor = "red";
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y;
@@ -88,6 +91,13 @@ class Game {
 
     if(player.distance > 3860){
       gameState = 2;
+    }
+
+    getRank(){
+      if(player.distance > 3860){
+        text("")
+      }
+      
     }
    
     drawSprites();
